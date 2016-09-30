2ad6278c2cf2c86d2503f693b19b4ce63ed099f1
Giuseppe Acito <giuseppe.acito@gmail.com>
2016-09-30 22:20:51 +0200

    Bump version: 0.1.5 → 0.2.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = [
 
 setup(
     name='diffservice',
-    version='0.1.5',
+    version='0.2.0',
     description='Service for compare Data and build reports',
     long_description=readme + '\n\n' + history,
     author='Giuseppe Acito',
